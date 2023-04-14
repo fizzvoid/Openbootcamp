be89a07e6c51cab706be76e44cd10ab997ba4710
fizzvoid <emmanuelcorralestorres@gmail.com>
2023-04-14 11:17:21 -0700

    corrigiendo nombres java

---
+++ Introduccion-a-la-programacion/Actividad8_introduccion_a_la_programacion.java
@@ -0,0 +1,37 @@
+public class Actividad8_introduccion_a_la_programacion {
+public static void main(String[] args) {
+    persona persona1 = new persona();
+    persona1.setEdad(18);
+    persona1.setNombre("Emmanuel");
+    persona1.setTelefono(662721393);
+    System.out.println(persona1.getEdad());
+    System.out.println(persona1.getNombre());
+    System.out.println(persona1.getTelefono());
+    
+}
+}
+class persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    
+}
