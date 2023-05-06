ef5b5785ffc7aade05714a515d945b3580032a72
fizzvoid <emmanuelcorralestorres@gmail.com>
2023-05-06 13:03:16 -0700

    agregando actividad 2 js

---
+++ Javascript_Basico/02__sintaxis-variables/index.js
@@ -0,0 +1,14 @@
+const listado = [
+    "emmanuel",
+    18,
+    true,
+    new Date(2004, 7, 20),
+    {
+        titulo: "el alquimista",
+        autor: "paulo cohelo",
+        fecha: new Date("1988"),
+        url: "https://www.amazon.com.mx/El-alquimista-Paulo-Coelho/dp/6073158602"
+        
+    }
+
+]
