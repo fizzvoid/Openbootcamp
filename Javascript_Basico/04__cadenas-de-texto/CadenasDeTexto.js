d3cec505429363ca4cc84e2204fe0d10a79e322f
fizzvoid <emmanuelcorralestorres@gmail.com>
2023-05-22 13:41:15 -0700

    agregando actiidad 4 js

---
+++ Javascript_Basico/04__cadenas-de-texto/CadenasDeTexto.js
@@ -0,0 +1,10 @@
+let nombre = `Emmanuel`;
+let apellido = `Corrales`;
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let estudianteLegth = estudiante.length;
+let primeraLetra = nombre.substring(0,1)
+let ultimaLetra = apellido.substring(apellido.length -1, apellido.length)
+let sinEspacio = estudiante.replace(/ /g,"")
+let tieneNombre = estudiante.includes(nombre)
