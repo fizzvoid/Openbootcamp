e92ab8e7354dc2cafaabca24eff9db980fbe4d54
fizzvoid <emmanuelcorralestorres@gmail.com>
2023-06-05 13:05:59 -0700

    agregando actividad 10 js

---
+++ Javascript_Basico/09__gestion-de-errores/index.js
@@ -0,0 +1,25 @@
+const { error } = require('console');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+function errorPersonalizado() {
+    throw new Error("soy un error")
+}
+try {
+    errorPersonalizado()
+} catch (e) {
+    logger.error("error", )
+}
